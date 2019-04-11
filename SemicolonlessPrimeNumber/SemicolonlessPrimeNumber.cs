6b27850e2827bee10dcf1927e6cb80a2950f2cbd
Takumi Yamamoto <takumi.yamamoto1989@gmail.com>
2019-04-12 02:02:22 +0900

    init commit

---
+++ SemicolonlessPrimeNumber/SemicolonlessPrimeNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Linq
+{
+    static class SemicolonlessPrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            if (Enumerable.Range(2, 1000000)
+            .Where(n => n == 2 || n % 2 != 0)
+            .Select(n => (int)Math.Sqrt(n) + 1 is var sqrt &&
+                            Enumerable.Range(0, sqrt)
+                            .Any(i => 2 < i && n % i == 0) ?
+                             new { value = n.ToString() + Environment.NewLine, IsPrime = false }
+                             : new { value = n.ToString() + Environment.NewLine, IsPrime = true })
+            .Where(x => x.IsPrime == true)
+            .Select(x => Console.OpenStandardOutput().WriteAsync(Text.Encoding.Default.GetBytes(x.value)))
+            .ToList() != null) { }
+        }
+    }
+}
